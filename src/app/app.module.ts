e3538fcf19129978a341bea096f6a12d2a0ce8f4
DESKTOP-9JG8FU1\Karlin <karlinkd010@gmail.com>
2021-08-08 00:16:49 -0500

    Creacion de estructura deÃl proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaisModule,
+    SharedModule
   ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
